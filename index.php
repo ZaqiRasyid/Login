f17c48428fdd119fe52852c79797f18c05836abe
ZaqiRasyid <zaqi.rasyid22@smk.belajar.id>
2025-04-11 14:07:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale="1">
     <title>game browser online</title>
      <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
   </head>
