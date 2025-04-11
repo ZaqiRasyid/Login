b95ffbbd87bff7a43032d47ac0eb95986d034dd9
ZaqiRasyid <zaqi.rasyid22@smk.belajar.id>
2025-04-11 14:36:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,30 @@
     </div>
   </div>
 </nav>
-
+<div class="container py-5">
+  <div class="row justify-content-center">
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body bg-light">
+          <div class="text-center">
+            <h5>Silakan Login disini</h5>
+          </div>
+          <form action="config/aksi_login.php" method="POST">
+            <label class="form-label">Username</label>
+            <input type="text" name="username" class="form-control" required>
+            <label class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" required>
+            <div class="d-grid mt-2">
+              <button class="btn btn-primary" type="submit" name="kirim">MASUK</button>
+            </div>
+          </form>
+          <hr>
+          <p>Belum punya akun? <a href="register.php">Daftar disini!</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>              
 
 <footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
   <p>&copy; game browser online </p>
