111e76b5ad3e0ae901926ee44a05317e78579381
ZaqiRasyid <zaqi.rasyid22@smk.belajar.id>
2025-04-12 08:21:54 +0700

    Update aksi_login.php

--- aksi_login.php
+++ aksi_login.php
@@ -1,54 +1,49 @@
-<?php
+<?php 
 session_start();
-include 'koneksi.php';
+require 'koneksi.php';
+$user=$_POST['nama'];
+$pass=md5($_POST['password']);
+$sql=mysqli_query($koneksi, "select * from user where nama='$user' and password='$pass'");
+$cek=mysqli_num_rows($sql);
 
-$username = $_POST['username'];
-$password = md5($_POST['password']);
-
-$sql = mysqli_query($koneksi,"SELECT * FROM user WHERE username='$username' AND password='$password'");
-
-$cek = mysqli_num_rows($sql);
-
-
-if ($cek > 0) {
-	$data = mysqli_fetch_array($sql);
-	if ($data['level']=="admin")
+	if ($cek>0)
 	{
-	session_start();
-	$_SESSION['username'] = $data['username'];
-	$_SESSION['userid'] = $data['userid'];
-	$_SESSION['status'] = 'login';
-	echo "<script>
-	alert('Login berhasil');
-	location.href='../admin/index.php';
-	</script>";
-	}else if ($data['level']=="pengembang")
+	$data=mysqli_fetch_array($sql);
+		if ($data['reason_at']=="admin")
+		{
+		$_SESSION['nama']=$data['nama'];
+		$_SESSION['user_id']=$data['user_id'];
+		$_SESSION['status'] = 'login';
+		echo "<script>
+		alert('Login berhasil');
+		location.href='admin.php';
+		</script>";
+		}
+		else if ($data['reason_at']=="pengembang") 
 	{
-	session_start();
-	$_SESSION['username'] = $data['username'];
-	$_SESSION['userid'] = $data['userid'];
-	$_SESSION['status'] = 'login';
-	echo "<script>
-	alert('Login berhasil');
-	location.href='../pengembang/index.php';
-	</script>";
-	}else if ($data['level']=="player")
+		$_SESSION['nama']=$data['nama'];
+		$_SESSION['user_id']=$data['user_id'];
+		$_SESSION['status'] = 'login';
+		echo "<script>
+		alert('Login berhasil');
+		location.href='pengembang.php';
+		</script>";
+		}
+		else if  ($data['reason_at']=="player") 
 	{
-	session_start();
-	$_SESSION['username'] = $data['username'];
-	$_SESSION['userid'] = $data['userid'];
-	$_SESSION['status'] = 'login';
-	echo "<script>
-	alert('Login berhasil');
-	location.href='../player/index.php';
-	</script>";	
-	}else
-	{
-	echo "<script>
+		$_SESSION['nama']=$data['nama'];
+		$_SESSION['user_id']=$data['user_id'];
+		$_SESSION['status'] = 'login';
+		echo "<script>
+		alert('Login berhasil');
+		location.href='user.php';
+		</script>";
+	}
+	else
+	{   echo "<script>
 	alert('Username atau Password anda salah!');
-	location.href='login.php';
+	location.href='index.php';
 	</script>";
 	}
 }
-
 ?>
